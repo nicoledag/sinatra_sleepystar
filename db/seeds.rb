884b58ad500033f9771f63a7ad91ebf058b0cbcf
Nicole D <nnardella@gmail.com>
2019-02-15 03:07:37 +0000

    Update date data in seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@ julie = Baby.create(name: "Julie", age: "15 months")
 alec = Baby.create(name: "Alec", age: "6 months")
 kristi = Baby.create(name: "Kristi", age: "6 months")
 
-julie_monday = Planner.create(day: "Monday", wake_time: "8am", bed_time: "8pm")
-julie_tuesday = Planner.create(day: "Tuesday", wake_time: "8:30am", bed_time: "8:30pm")
+julie_monday = Planner.create(date: "2019-2-11", day: "Monday", wake_time: "8am", bed_time: "8pm")
+julie_tuesday = Planner.create(date: "2019-2-12", day: "Tuesday", wake_time: "8:30am", bed_time: "8:30pm")
 
 alec_monday = Planner.create(day: "Monday", wake_time: "7:30am", bed_time: "8pm")
 alec_tuesday = Planner.create(day: "Tuesday", wake_time: "7:30am", bed_time: "8pm")
