db3d8b7c8a15d40aceffe91523d6772d336d07d3
Nicole D <nnardella@gmail.com>
2019-02-15 02:26:08 +0000

    Create seed data

---
+++ db/migrate/seeds.rb
@@ -0,0 +1,16 @@
+
+nicole = User.create(username: "Nicole", password_digest: "hello767")
+sara = User.create(username: "Sara", password_digest: "whatsup944")
+
+julie = Baby.create(name: "Julie", age: "15 months")
+alec = Baby.create(name: "Alec", age: "6 months")
+kristi = Baby.create(name: "Kristi", age: "6 months")
+
+julie_monday = Planner.create(day: "Monday", wake_time: "8am", bed_time: "8pm")
+julie_tuesday = Planner.new(day: "Tuesday", wake_time: "8:30am", bed_time: "8:30pm")
+
+alec_monday = Planner.create(day: "Monday", wake_time: "7:30am", bed_time: "8pm")
+alec_tuesday = Planner.new(day: "Tuesday", wake_time: "7:30am", bed_time: "8pm")
+
+kristi_monday = Planner.create(day: "Monday", wake_time: "6:30am", bed_time: "7pm")
+kristi_tuesday = Planner.new(day: "Tuesday", wake_time: "6:30am", bed_time: "7pm")
