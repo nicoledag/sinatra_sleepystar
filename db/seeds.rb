d7425b1e44f40d34410a03aba77a478d7f17aae5
Nicole D <nnardella@gmail.com>
2019-02-19 04:30:00 +0000

    Test objects in Tux

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
-nicole = babies.create(username: "Nicole", password: "hello767")
+nicole = User.create(username: "Nicole", password: "hello767")
 sara = User.create(username: "Sara", password: "whatsup944")
 
 julie = Baby.create(name: "Julie", age: "15 months", user_id: nicole.id)
