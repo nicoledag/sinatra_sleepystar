dce7caf9f709a7c20106842bbab0c5dd6c821da2
Nicole D <nnardella@gmail.com>
2019-02-19 04:15:59 +0000

    Update seed.rb to create objects

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,31 @@
 
-@nicole = babies.create(username: "Nicole", password: "hello767")
+nicole = babies.create(username: "Nicole", password: "hello767")
 sara = User.create(username: "Sara", password: "whatsup944")
 
-julie = Baby.create(name: "Julie", age: "15 months")
-alec = Baby.create(name: "Alec", age: "6 months")
-kristi = Baby.create(name: "Kristi", age: "6 months")
+julie = Baby.create(name: "Julie", age: "15 months", user_id: nicole.id)
+alec = Baby.create(name: "Alec", age: "6 months", user_id: nicole.id)
+kristi = Baby.create(name: "Kristi", age: "6 months", user_id: sara.id)
 
-julie_monday = Planner.create(date: "2019-2-11", day: "Monday", wake_time: "8am", bed_time: "8pm")
-julie_tuesday = Planner.create(date: "2019-2-12", day: "Tuesday", wake_time: "8:30am", bed_time: "8:30pm")
+julie_monday = Planner.create(date: "2019-2-11", day: "Monday", wake_time: "8am", bed_time: "8pm", baby_id: julie.id)
+julie_tuesday = Planner.create(date: "2019-2-12", day: "Tuesday", wake_time: "8:30am", bed_time: "8:30pm", baby_id: julie.id)
 
-alec_monday = Planner.create(day: "Monday", wake_time: "7:30am", bed_time: "8pm")
-alec_tuesday = Planner.create(day: "Tuesday", wake_time: "7:30am", bed_time: "8pm")
+alec_monday = Planner.create(day: "Monday", wake_time: "7:30am", bed_time: "8pm", baby_id: alec.id)
+alec_tuesday = Planner.create(day: "Tuesday", wake_time: "7:30am", bed_time: "8pm", baby_id: alec.id)
 
-kristi_monday = Planner.create(day: "Monday", wake_time: "6:30am", bed_time: "7pm")
-kristi_tuesday = Planner.create(day: "Tuesday", wake_time: "6:30am", bed_time: "7pm")
+kristi_monday = Planner.create(day: "Monday", wake_time: "6:30am", bed_time: "7pm", baby_id: kristi.id)
+kristi_tuesday = Planner.create(day: "Tuesday", wake_time: "6:30am", bed_time: "7pm", baby_id: kristi.id)
 
 # User
 # has_many :babies
 # #parent class
 # #user.babies
-#
+
 # has_many :planners, through: :babies
 # #parent class
 # #user.babies.planners
 
 # Baby
-# belongs_to :user
+# belongs_to :user (user_id)
 # #refered to as single term
 # #baby.users
 # has_many :planners
@@ -33,10 +33,7 @@ kristi_tuesday = Planner.create(day: "Tuesday", wake_time: "6:30am", bed_time: "
 # # baby.planners
 
 
-
-
-
 # Planner
-# belongs_to :baby
+# belongs_to :baby (baby_id)
 #refers to as single terms.
 #planner.babies
