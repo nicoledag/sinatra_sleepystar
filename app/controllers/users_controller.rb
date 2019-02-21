2339b65cd62ad532ba17e9dae8988e58123c2ab3
Nicole D <nnardella@gmail.com>
2019-02-21 03:00:51 +0000

    Work on post login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,21 @@ class UsersController < ApplicationController
   end
 
   post '/login' do
-    binding.pry
+    @user = User.find_by(email: params[:email])
+    if @user.authenticate(params[:password])
+      session[:user_id] = @user_id #logging user in
+      puts session
+      redirect to "/users/#{user.id}"
+    else
+
+    end
   end
 
   get '/signup' do
 
+  end
+
+  get '/users/:id' do
+    "this will be the users show route"
   end
 end
