4594608d4c1c04e8a33a065be587fd493e159d67
Nicole D <nnardella@gmail.com>
2019-02-21 03:00:17 +0000

    Enable sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,9 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "the_best_sleep_planner"  #creates session ID for particular session.  Extra layer of security.
+
   end
 
   get "/" do
