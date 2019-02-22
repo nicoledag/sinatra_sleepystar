3608d0a667140330bff26956cab8992bbcbe3168
Nicole D <nnardella@gmail.com>
2019-02-22 00:04:44 +0000

    Update seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,18 +2,18 @@
 nicole = User.create(username: "Nicole", password: "hello767")
 sara = User.create(username: "Sara", password: "whatsup944")
 
-julie = Baby.create(name: "Julie", age: "15 months", user_id: nicole.id)
-alec = Baby.create(name: "Alec", age: "6 months", user_id: nicole.id)
-kristi = Baby.create(name: "Kristi", age: "6 months", user_id: sara.id)
+JulieBaby.create(name: "Julie", age: "15 months", user_id: nicole.id)
+AlecBaby.create(name: "Alec", age: "6 months", user_id: nicole.id)
+KristiBaby.create(name: "Kristi", age: "6 months", user_id: sara.id)
 
-julie_monday = Planner.create(date: "2019-2-11", day: "Monday", wake_time: "8am", bed_time: "8pm", baby_id: julie.id)
-julie_tuesday = Planner.create(date: "2019-2-12", day: "Tuesday", wake_time: "8:30am", bed_time: "8:30pm", baby_id: julie.id)
+JulieMondayPlanner.create(date: "2019-2-11", day: "Monday", wake_time: "8am", bed_time: "8pm", baby_id: julie.id)
+JulieTuesdayPlanner.create(date: "2019-2-12", day: "Tuesday", wake_time: "8:30am", bed_time: "8:30pm", baby_id: julie.id)
 
-alec_monday = Planner.create(day: "Monday", wake_time: "7:30am", bed_time: "8pm", baby_id: alec.id)
-alec_tuesday = Planner.create(day: "Tuesday", wake_time: "7:30am", bed_time: "8pm", baby_id: alec.id)
+AlecMondayPlanner.create(day: "Monday", wake_time: "7:30am", bed_time: "8pm", baby_id: alec.id)
+AlecTuesdayPlanner.create(day: "Tuesday", wake_time: "7:30am", bed_time: "8pm", baby_id: alec.id)
 
-kristi_monday = Planner.create(day: "Monday", wake_time: "6:30am", bed_time: "7pm", baby_id: kristi.id)
-kristi_tuesday = Planner.create(day: "Tuesday", wake_time: "6:30am", bed_time: "7pm", baby_id: kristi.id)
+KristiMondayPlanner.create(day: "Monday", wake_time: "6:30am", bed_time: "7pm", baby_id: kristi.id)
+KristiTuesdayPlanner.create(day: "Tuesday", wake_time: "6:30am", bed_time: "7pm", baby_id: kristi.id)
 
 # User
 # has_many :babies
