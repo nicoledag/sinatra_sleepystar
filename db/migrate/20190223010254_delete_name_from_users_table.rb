b9e0ba71bf7ad82a4810fb9e39fddb300b181c19
Nicole D <nnardella@gmail.com>
2019-02-23 01:04:17 +0000

    Delete name attribute from user

---
+++ db/migrate/20190223010254_delete_name_from_users_table.rb
@@ -0,0 +1,5 @@
+class DeleteNameFromUsersTable < ActiveRecord::Migration
+  def change
+    remove_column :users, :name, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190222024408) do
+ActiveRecord::Schema.define(version: 20190223010254) do
 
   create_table "babies", force: :cascade do |t|
     t.string   "name"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 20190222024408) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
-    t.string   "name"
   end
 
 end
