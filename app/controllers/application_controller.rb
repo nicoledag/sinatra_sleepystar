4153831a39fdd22521af809cd11a136a91283868
Nicole D <nnardella@gmail.com>
2019-02-23 01:15:53 +0000

    Update helper methods in applicaton_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,11 +16,16 @@ class ApplicationController < Sinatra::Base
 
   helpers do
     def logged_in?
-      !!session[:user_id]
+      !!current_user
+      # A "double-bang operator" (!!) will return true or false based on whether a value is truthy or falsey to begin with.
     end
 
     def current_user
-      User.find(session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
+      #reduce a few database calls.  memorization
+      #create instance variable @current_user within scope of application controller.
+      #the first time current_user is referenced and user is found.
+      #subsequent call if already populated won't hit database again
     end
   end
 
