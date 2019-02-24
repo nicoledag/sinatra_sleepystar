04dbe46f28b17018fbd4ffa813cfe07dc09c2dc5
Nicole D <nnardella@gmail.com>
2019-02-24 03:48:50 +0000

    Change post users to post signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -32,7 +32,7 @@ class UsersController < ApplicationController
   end
 
 
-  post '/users' do
+  post '/signup' do
     user = User.new(:username => params[:username], :password => params[:password])
     #only persist a user that has a name and password.
     #check if a user already exists when signing in so user isn't duplicated?
