7bf2d51850fc13925f9e2364798e6f818b81952c
Nicole D <nnardella@gmail.com>
2019-02-27 18:30:52 +0000

    Add notes to plan out controller routes needed

--- app/controllers/planners_controllers.rb
+++ app/controllers/planners_controllers.rb
@@ -1,7 +1,21 @@
 class PlannersController < ApplicationController
 
+
+  # get planners/new to render a form to create a new entry.
   get '/planners' do
-    
+
   end
+
+  #post planners to create a new planner entry.
+
+  #show route for a planner entry.
+
+  #index route for all planner entries.
+
+  #get planners/:id/edit to render form to edit a planner.
+
+  #patch planners to update a planner.
+
+  #delete planners to delete a planner.
 
 end
