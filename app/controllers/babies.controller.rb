f3cb4735467cb4184e708ef0f127b01a2161ee8f
Nicole D <nnardella@gmail.com>
2019-02-27 18:30:32 +0000

    Add notes to plan out controller  routes needed

--- app/controllers/babies.controller.rb
+++ app/controllers/babies.controller.rb
@@ -1,5 +1,21 @@
 class BabiesController < ApplicationController
 
-  
+  # get babies/new to render a form to create a new entry.
+  get '/babies' do
+
+  end
+
+  #post babies to create a new baby entry.
+
+  #show route for a baby entry.
+
+  #index route for all baby entries.
+
+  #get babies/:id/edit to render form to edit a baby.
+
+  #patch babies to update a baby.
+
+  #delete babies to delete a baby.
+
 
 end
