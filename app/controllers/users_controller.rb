66c06ce4cc789f070f033f96c5842e35ec76d467
Nicole D <nnardella@gmail.com>
2019-03-01 15:55:41 +0000

    Update post signup to find_by params email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   post '/signup' do
     # PREVENT SOMEONE SIGNING UP TWICE WITH THE SAME USERNAME.
     user = User.find_by(:username => params[:username])
+    binding.pry
 
     if user != nil
       redirect '/login'
@@ -36,7 +37,7 @@ class UsersController < ApplicationController
   post '/login' do
     user = User.find_by(:username => params[:username])
 
-    if user && user.authenticate(params[:password]) && user.username != "" 
+    if user && user.authenticate(params[:password]) && user.username != ""
       session[:user_id] = user.id
 
       redirect "planners"
