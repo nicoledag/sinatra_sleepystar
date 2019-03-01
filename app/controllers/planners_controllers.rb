f7afdd1a896f44f5acd507a3e18a5796489bb13f
Nicole D <nnardella@gmail.com>
2019-03-01 18:52:15 +0000

    Start planners new erb form

--- app/controllers/planners_controllers.rb
+++ app/controllers/planners_controllers.rb
@@ -11,8 +11,12 @@ class PlannersController < ApplicationController
   end
 
   # get planners/new to render a form to create a new entry.
-  get 'planners/new' do
-    
+  get '/planners/new' do
+    if logged_in?
+     erb :'/planners/new'
+    else
+     redirect '/login'
+   end
   end
 
   #post planners to create a new planner entry.
