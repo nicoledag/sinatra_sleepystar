20d0e322f69bd74c30ae46f8059030622d73cb8e
Nicole D <nnardella@gmail.com>
2019-03-01 15:48:25 +0000

    Update email attribute in users controller and create_user form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -36,7 +36,7 @@ class UsersController < ApplicationController
   post '/login' do
     user = User.find_by(:username => params[:username])
 
-    if user && user.authenticate(params[:password]) && user.username != "" && params[:password] != ""
+    if user && user.authenticate(params[:password]) && user.username != "" 
       session[:user_id] = user.id
 
       redirect "planners"
