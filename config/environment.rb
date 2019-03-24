c24375bf9a0119fb68ed57b10363604d4f952033
github username <github email address>
2019-03-23 22:52:24 -0400

    postgres

--- config/environment.rb
+++ config/environment.rb
@@ -1,15 +1,47 @@
+#
+#
+# ENV['SINATRA_ENV'] ||= "development"
+#
+# require 'bundler/setup'
+# Bundler.require(:default, ENV['SINATRA_ENV'])
+#
+# # ActiveRecord::Base.establish_connection(
+# #   :adapter => "sqlite3",
+# #   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+# # )
+#
+# ActiveRecord::Base.establish_connection(
+#   ENV['DATABASE_URL'] || 'postgres://localhost/sleepy_star'
+# )
+#
+#
+# require './app/controllers/application_controller'
+# require_all 'app'
 
-
-ENV['SINATRA_ENV'] ||= "development"
 
 require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+Bundler.require
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
-)
+configure :development do
+  ENV['SINATRA_ENV'] ||= "development"
 
+  ActiveRecord::Base.establish_connection(
+    :adapter => "sqlite3",
+    :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
+end
 
-require './app/controllers/application_controller'
+configure :production do
+  db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+    :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
+
 require_all 'app'
